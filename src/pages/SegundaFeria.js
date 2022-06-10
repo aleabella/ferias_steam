57b840b4c420d2322b4652eed983f7b89bab1177
aleabella <98822208+aleabella@users.noreply.github.com>
2022-06-10 11:36:24 -0500

    addign more things

--- src/pages/SegundaFeria.js
+++ src/pages/SegundaFeria.js
@@ -1,10 +1,12 @@
 import Footer from "../complements/Footer/Footer";
 import Header from "../complements/Header/Header";
+import Dos from "../complements/dos/Dos";
 
 function SegundaFeria (){
     return (
         <>
             <Header/>
+            <Dos/>
             <Footer/>
         </>
     )
